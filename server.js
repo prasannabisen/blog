d3de9ebbefbbab8df0648f0c1ec8d937a3fa963d
montu <montu@gmail.com>
2019-08-03 00:19:10 +0530

    .

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express=require('express')
 const app=express()
 const {db,sequelize}=require('./db')
+
 app.set('view engine','hbs')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
+
+module.exports={
+    app
+}
 
 db.sync().then(()=>{
     app.listen(3000)
