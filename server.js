11708aaeec80e16ce5b749c8645f6bf5ffb7ff79
montu <montu@gmail.com>
2019-08-03 00:52:27 +0530

    .

--- server.js
+++ server.js
@@ -11,10 +11,27 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
+app.get('/add',(req,res)=>{
+    res.render('add')
+})
+
+app.post('/add',(req,res)=>{
+    db.create({
+        idNo:req.body.idNo,
+        head:req.body.head,
+        date:req.body.date,
+        info:req.body.info
+    }).then(()=>{
+        res.redirect('https://google.com')
+    })
+})
+
+db.sync().then(()=>{
+    app.listen(4742,()=>{
+        console.log('connecteds')
+    })
+})
+
 module.exports={
     app
 }
-
-db.sync().then(()=>{
-    app.listen(3000)
-})
